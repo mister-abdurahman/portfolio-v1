801323062172eb300db275e68052c71c037c5306
r.aramide <r.aramide@lagetronix.com>
2023-09-25 17:02:47 +0100

    added images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        DMSans: "DM Sans",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      backgroundColor: {
+        primary: "#edf2f8",
+        secondary: "#313bac",
+        black: "#030303",
+        brown: "#46364a",
+        white: "#ffffff",
+        transparent_gray: "rgba(255, 255, 255, 0.25)",
+        lightGray: "#e4e4e4",
+      },
+      textColor: {
+        primary: "#edf2f8",
+        secondary: "#313bac",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
